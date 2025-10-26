aa839e6b38fb8dabae712130b42485443fe2139f
VIBOL <keasokvibol@gmail.com>
2025-10-26 08:16:36 +0700

    Modified code in contact.php

--- contact.php
+++ contact.php
@@ -1,5 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <metra charset="UTF-8">
-        <metra name="viewport" content="wid
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact</title>
+</head>
+<body>
+    <form action="">
+        Username: <input type="text">
+        Email: <input type="email">
+        <input type="submit" value="Send">
+    </form>
+</body>
+</html>
